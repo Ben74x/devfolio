4a25daf926e285ef8b03cae61070d9863deb61e4
Benjamin Dwumah <bdwumah74@gmail.com>
2022-07-08 18:03:21 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://bdwumah.netlify.app/`,
+    siteUrl: `https://peaceful-trifle-61538a.netlify.app/`,
     // Your Name
     name: 'Ryan Fitzgerald',
     // Main Site Title
