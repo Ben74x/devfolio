955ee55a9bf3846c2c1c35106b0b26cc2e3180bf
Benjamin Dwumah <bdwumah74@gmail.com>
2022-09-18 20:43:51 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     // Optional: LinkedIn account URL
     linkedin: `https://www.linkedin.com/in/benjamin-dwumah74/`,
     // Content of the About Me section
-    about: `I'm a motivated DevOps Engineer with experience in automating, architecting and optimizing mission critical deployments over various infrastructure. I'm also passionate about AI and I share my progress and thoughts through my github and blog. I love to chat about Python, DevOps & Cloud practices, Machine Learning and Anime. You can reach out to me on my LinkedIn`,
+    about: `I'm a motivated DevOps Engineer with experience in automating, architecting and optimizing mission critical deployments over various infrastructure. I'm also passionate about AI and I share my progress and thoughts through my github and blog. I love to chat about Python, DevOps & Cloud practices, Machine Learning and Anime. You can reach out to me on my LinkedIn.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
